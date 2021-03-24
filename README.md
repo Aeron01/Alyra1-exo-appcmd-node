03e92cc2a79e1839a9afe07f1174733fdcb3d111
Aeron01 <one.soulreaper@gmail.com>
2021-03-24 17:53:40 +0100

    little update

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # test-new-project
+
+Ce programme crée un nouveau répertoir, un nouveau fichier vide, et d'y ajouter automatiquement dans ce dernier, une string.
+
+Ce programme est concue pour ne créer que le fichier, si celui-ci n'existe pas déjà et si le répertoir est déjà existant et le chemin valide.
+il peut aussi créer un répertoir, si le chemin fournis est existant (ex: ./).
+
+Utilisation : node create-it.js path dir_name file_name string
--- create-it.js
+++ create-it.js
@@ -1,7 +1,9 @@
-/* programe qui devra :
-1 creer un repertoir d'on le nom qu'on aura entrer en parametre 1
-2 creer dans ce repertoir, un fichier (.js pour mon exemple) d'on le nom qu'on aura entrer en parametre 2
-3 devra ecrire une string dans le fichier qui sera entrer dans le parametre 3
+/* Ce programme qui sera lancer en ligne de commande du terminal devra :
+1: Créer un dossier d'ont le chemin et le nom seront définie par l'utilisateur (parametre 1 pour le chemin et parametre 2 pour le nom de dossier)
+2: Créer dans ce dossier, un fichier d'ont le nom sera défini par l'utilisateur et sera le parametre 3 
+3: Devra écrire une string dans le fichier qui sera entrer en parametre 4
+4: Vérifier avant tout que le chemin ou le dossier et le fichier n'existe pas déjà.
+5: Si le chemin et le dossier sont existant, mais pas le fichier, le créer. Sinon quitter le programme en affichant un message.
 */
 
 const chalk = require('chalk');
@@ -17,9 +19,19 @@ const nameDir = process.argv[3]
 const nameFil = process.argv[4]
 const str = process.argv[5]
 
-if (existsSync(`${path}${nameDir}` || `${nameFil}`)) {
-  console.log(chalk.redBright(`The path or directory or filename as already exists.`))
-  process.exit(1)
+if (existsSync(`${path}${nameDir}`)) {
+  if (existsSync(`${path}${nameDir}/${nameFil}`)) {
+    console.log(chalk.redBright(`The path or directory or filename as already exists.`))
+    process.exit(1)
+  } else {
+    console.log(chalk.yellowBright(`Creating empty file ${nameFil} in directory ${nameDir}`))
+    closeSync(openSync(`${nameDir}/${nameFil}`, 'w'))
+    console.log(chalk.greenBright('Done !'))
+    console.log(chalk.yellowBright(`Writing in file the string ${str}`))
+    writeFileSync(`${nameDir}/${nameFil}`, `${str}`)
+    console.log(chalk.greenBright('Done !'))
+    console.log(chalk.blueBright(`All step Done. According to your request you will find the file in the following path: ${path}${nameDir}/${nameFil}`))
+  }
 } else {
   console.log(chalk.yellowBright(`Creating directory in ${path}${nameDir}`))
   mkdirSync(`${path}${nameDir}`)
@@ -30,7 +42,7 @@ if (existsSync(`${path}${nameDir}` || `${nameFil}`)) {
   console.log(chalk.yellowBright(`Writing in file the string ${str}`))
   writeFileSync(`${nameDir}/${nameFil}`, `${str}`)
   console.log(chalk.greenBright('Done !'))
-  console.log(`All step Done. according to your request you will find the file in the following path: ${path}${nameDir}/${nameFil}`)
+  console.log(chalk.blueBright(`All step Done. According to your request you will find the file in the following path: ${path}${nameDir}/${nameFil}`))
 }
 
 
